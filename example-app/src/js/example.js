020b833e5def1f8091812dfb2b27dafd278a9027
Eduardo Roth <eduardo@eduardoroth.dev>
2024-11-12 17:53:18 -0600

    chore(): remove example app

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { MediaPlayer } from '@eduardoroth&#x2F;media-player';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    MediaPlayer.echo({ value: inputValue })
-}
